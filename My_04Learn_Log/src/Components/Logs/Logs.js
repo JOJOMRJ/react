0861a21d98bdff30172a63ac31ec29c02b555ada
JoJo <jiaoyalin92@gmail.com>
2024-11-19 10:07:12 +1100

    完成空日志提示

--- My_04Learn_Log/src/Components/Logs/Logs.js
+++ My_04Learn_Log/src/Components/Logs/Logs.js
@@ -3,11 +3,22 @@ import "./Logs.css"
 import Card from "../UI/Card/Card";
 // 日志的容器
 const Logs = (props) => {
-  const logItemData = props.logsData.map((item,index) => <LogItems onDelLog={()=>props.onDelLog(index)} key={item.id} date={item.date} desc={item.desc} time={item.time}/>)
+  let logItemData = props.logsData.map((item,index) => <LogItems 
+                                                                onDelLog={()=>props.onDelLog(index)} 
+                                                                key={item.id} 
+                                                                date={item.date} 
+                                                                desc={item.desc} 
+                                                                time={item.time}/>)
+  if(logItemData.length === 0) {
+    logItemData = <p className="no-logs">请添加日志</p>
+  }
   return (
     //   把className 传给Card
     <Card className="logs">
-      {logItemData}
+      {
+        // logItemData.length !== 0 ? logItemData : <p className="no-logs">请添加日志</p>
+        logItemData
+      }
     </Card>
   )}
 
